98c577c4fa9a6f8b23c6c4bdcbe8b62102b0c36d
Coder-Mehedi <mehedi5051@gmail.com>
2023-09-05 22:33:18 +0600

    ongoing test tsconfig

--- @types/index.d.ts
+++ @types/index.d.ts
@@ -1,23 +1,57 @@
-type SDKInitInput = {
-  sdk_type: string;
+type CommonInput = {
   client_id: string;
   client_secret: string;
   application_id: string;
   organization_id: string;
 };
 
-type LoginInput = {
+type SDKInitInput = CommonInput & {
+  sdk_type: string;
+};
+
+type LoginInput = CommonInput & {
   type: string;
   response_type: string;
-  organization_id: string;
   username: string;
   password: string;
   email: string;
   phone: string;
   region: string;
-  application_id: string;
-  client_id: string;
-  client_secret: string;
   redirect_uri: string;
   country_code: string;
+};
+
+type RegisterInput = CommonInput &
+  Omit<LoginInput, 'response_type'> & {
+    metadata: {
+      [key: string]: any;
+    };
+    sdk_type: string;
+    name: string;
+    first_name: string;
+    last_name: string;
+    affiliation: string;
+    id_card: string;
+    provider: string;
+    code: string;
+    state: string;
+    method: string;
+    email_code: string;
+    phone_code: string;
+    auto_sign_in: boolean;
+    relay_state: string;
+    captcha_type: string;
+    captcha_token: string;
+    mfa_type: string;
+    passcode: string;
+    recovery_code: string;
+  };
+
+type LogoutInput = CommonInput & {
+  refresh_token: string;
+};
+
+type RefreshTokenInput = CommonInput & {
+  refresh_token: string;
+  grant_type: string;
 };
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import axios from 'axios';
+import api from './lib/api';
 
-const api = axios.create({
-  baseURL: 'http://localhost:9111',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-});
+// const api = axios.create({
+//   baseURL: 'http://localhost:9111',
+//   headers: {
+//     'Content-Type': 'application/json',
+//   },
+// });
 
 const sdkInitApi = async (sdkInitInput: SDKInitInput) => {
   try {
@@ -50,64 +51,6 @@ const registerApi = async (registerInput: RegisterInput) => {
   } catch (error) {
     console.log(error);
   }
-};
-
-type CommonInput = {
-  client_id: string;
-  client_secret: string;
-  application_id: string;
-  organization_id: string;
-};
-
-type SDKInitInput = CommonInput & {
-  sdk_type: string;
-};
-
-type LoginInput = CommonInput & {
-  type: string;
-  response_type: string;
-  username: string;
-  password: string;
-  email: string;
-  phone: string;
-  region: string;
-  redirect_uri: string;
-  country_code: string;
-};
-
-type RegisterInput = CommonInput &
-  Omit<LoginInput, 'response_type'> & {
-    metadata: {
-      [key: string]: any;
-    };
-    sdk_type: string;
-    name: string;
-    first_name: string;
-    last_name: string;
-    affiliation: string;
-    id_card: string;
-    provider: string;
-    code: string;
-    state: string;
-    method: string;
-    email_code: string;
-    phone_code: string;
-    auto_sign_in: boolean;
-    relay_state: string;
-    captcha_type: string;
-    captcha_token: string;
-    mfa_type: string;
-    passcode: string;
-    recovery_code: string;
-  };
-
-type LogoutInput = CommonInput & {
-  refresh_token: string;
-};
-
-type RefreshTokenInput = CommonInput & {
-  refresh_token: string;
-  grant_type: string;
 };
 
 class Access {
