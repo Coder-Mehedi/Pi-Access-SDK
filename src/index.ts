d67d0b266bafe510a28ff5a0b16d03c5e6d2b05a
Coder-Mehedi <mehedi5051@gmail.com>
2023-09-05 16:40:11 +0600

    register api working

--- src/index.ts
+++ src/index.ts
@@ -43,6 +43,15 @@ const refreshTokenApi = async (refreshTokenInput: RefreshTokenInput) => {
   }
 };
 
+const registerApi = async (registerInput: RegisterInput) => {
+  try {
+    const res = await api.post('/auth/signup', registerInput);
+    return res.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 type CommonInput = {
   client_id: string;
   client_secret: string;
@@ -65,6 +74,32 @@ type LoginInput = CommonInput & {
   redirect_uri: string;
   country_code: string;
 };
+
+type RegisterInput = CommonInput &
+  Omit<LoginInput, 'response_type'> & {
+    metadata: {
+      [key: string]: any;
+    };
+    sdk_type: string;
+    name: string;
+    first_name: string;
+    last_name: string;
+    affiliation: string;
+    id_card: string;
+    provider: string;
+    code: string;
+    state: string;
+    method: string;
+    email_code: string;
+    phone_code: string;
+    auto_sign_in: boolean;
+    relay_state: string;
+    captcha_type: string;
+    captcha_token: string;
+    mfa_type: string;
+    passcode: string;
+    recovery_code: string;
+  };
 
 type LogoutInput = CommonInput & {
   refresh_token: string;
@@ -92,6 +127,10 @@ class Access {
   static async init(input: SDKInitInput): Promise<Access> {
     const initRes = sdkInitApi(input);
     return new Access({ ...initRes, ...input });
+  }
+
+  async register(registerInput: RegisterInput) {
+    return await registerApi(registerInput);
   }
 
   async login(loginInput: LoginInput) {
@@ -140,13 +179,52 @@ const main = async () => {
 
   const refreshTokenInput: RefreshTokenInput = {
     ...commonInput,
-    refresh_token:
-      'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJVc2VySUQiOiI3Njk2ZDg1MC0zNjllLTRkM2YtYjgyMi1mZDgxNmY0OTlmOWYiLCJ0b2tlblR5cGUiOiJyZWZyZXNoLXRva2VuIiwiQXBwbGljYXRpb24iOiI1M2Q3NTliMi1mZjM4LTRhM2MtYmZiNC05OTE5OGJhMTU4NmUiLCJBcHBsaWNhdGlvblVzZXJJRCI6Ijc2OTZkODUwLTM2OWUtNGQzZi1iODIyLWZkODE2ZjQ5OWY5ZiIsIkVtYWlsIjoiaGFzYW5AeW9wbWFpbC5jb20iLCJhdWQiOiI1M2Q3NTliMi1mZjM4LTRhM2MtYmZiNC05OTE5OGJhMTU4NmUiLCJleHAiOjE2OTQ1MDAxMzAsImp0aSI6Ijc2OTZkODUwLTM2OWUtNGQzZi1iODIyLWZkODE2ZjQ5OWY5ZiIsImlhdCI6MTY5Mzg5NTMzMCwiaXNzIjoiNTNkNzU5YjItZmYzOC00YTNjLWJmYjQtOTkxOThiYTE1ODZlIiwibmJmIjoxNjkzODk1MzMwLCJzdWIiOiI3Njk2ZDg1MC0zNjllLTRkM2YtYjgyMi1mZDgxNmY0OTlmOWYifQ.RhSaHN7yWA2gMWGqwWWkUyybOORn8Vy3EvXGZn3YDTIpQY4Vq6uocjtm1LTqze7cxxxMNvveZhEABBoJIPPoAlMNN7mNFAUlmEyAPUCwLXAzLGPHxMfCwWESuWntXCSoqYxoQF530WAoY1obgF_44Uuv_YJrFV2a3y3FYVaM_ZkxoSzK6sZRPkNDCa0ZAcNcxxzRpjPSgTOHjfyHm9evo3_BfLAVWAam_BcMQ1XJUG4Iqb_-6qPwDVLhn_rdkPd1NwnxEhLGPe5bEzQLXjIeMOXAm3x3WmnpIshxjEncF241z_GOh7ebeI7aYkR2hcc29HZtTh4Jh31vRgYBq3_8T5DK9pdRBttidd2p4MQvEKZgWOMR487JA13mMDtKJq6l_GLBgt_wo1ek6g9gJymj972AfsmOr7dfKzWb8HLuAFBUDvlFHzmMufSH6obPKLCshx4abfldse1d3Pzy1n4uuuyOnv8Wociyr4WofjAfnKdFv5phGlBsy250BtqRk9gN7UoY8ip1KCfLg4keRoB-0Ti1P6XYNagrWvtvQ3WqLDhvXUmUpLksUOLoFyFuZXUmFRpFD75nOMoy-jfh1k4YerIzIrJ-fnceBTr9Nk-wxvzK4TfXsDoz__LfFCy1KMTDNnMB7otTTplLlNK0OkXhsRqra3gnmHX-FnFMOK0FbxE',
+    refresh_token: '',
     grant_type: 'refresh_token',
   };
 
-  const refreshTokenRes = await client.refreshToken(refreshTokenInput);
-  console.log(refreshTokenRes);
+  const registerInput: RegisterInput = {
+    ...commonInput,
+
+    metadata: {
+      name: 'onimesh mitra2',
+      profile_picture: 'somecdnlink',
+      has_business: false,
+    },
+    sdk_type: '',
+    type: '',
+    username: 'mehedi1',
+    password: 'mehedi',
+    name: 'Mehedi Hasan',
+    first_name: 'Mehedi',
+    last_name: 'Hasan',
+    email: 'mehedi1@yopmail.com',
+    phone: '1479503553',
+    affiliation: '',
+    id_card: '',
+    region: '',
+    provider: '',
+    code: '',
+    state: '',
+    redirect_uri: 'https://example.com/callback',
+    method: 'POST',
+    email_code: '',
+    phone_code: '',
+    country_code: 'BD',
+    auto_sign_in: true,
+    relay_state: '',
+    captcha_type: '',
+    captcha_token: '',
+    mfa_type: '',
+    passcode: '',
+    recovery_code: '',
+  };
+
+  const registerRes = await client.register(registerInput);
+  console.log(registerRes);
+
+  // const refreshTokenRes = await client.refreshToken(refreshTokenInput);
+  // console.log(refreshTokenRes);
 
   // const logoutRes = await client.logout(logoutInput);
   // console.log(logoutRes);
@@ -162,41 +240,3 @@ const main = async () => {
 };
 
 main();
-
-// i want to create a npm package that will be used in react/next js project to authenticate user and get user data from the server
-//
-// 1. create a npm package
-
-// sdk init with client
-// type Config struct {
-// 	ClientId       string `validate:"notempty"`
-// 	ClientSecret   string `validate:"notempty"`
-// 	ApplicationId  string `validate:"notempty"`
-// 	OrganizationId string `validate:"notempty"`
-// 	Url            string `validate:"notempty"`
-// }
-
-// /sdk-init
-
-// {
-//   "sdk_type": "frontend",
-//   "client_id": "14a6b3d833",
-//   "client_secret": "03d102b3c701c7113b80",
-//   "application_id": "004f4f00-e014-46e5-bcd7-9794591ec064",
-//   "organization_id": "f614aab9-36a9-426b-848d-c23ce0370434"
-// }
-//       "application_name": "DEFAULT_APPLICATION",
-// "organization_name": "new_test",
-
-// type SdkConfig struct {
-// 	SdkType        string `json:"sdk_type"`
-// 	ClientId       string `json:"client_id"`
-// 	ClientSecret   string `json:"client_secret"`
-// 	ApplicationId  string `json:"application_id"`
-// 	OrganizationId string `json:"organization_id"`
-// }
-
-// "organization_id": "8ad5f6ae-6dbc-4ff0-a1d2-0234344166dd",
-// "application_id": "53d759b2-ff38-4a3c-bfb4-99198ba1586e",
-// "client_id": "b2aca2edb9",
-// "client_secret": "302479106f4fc38d9ee4",
